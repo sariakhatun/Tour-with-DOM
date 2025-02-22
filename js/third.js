b6e83af500f8a130738c70a8a2918201c0a543f3
Saria Khatun <sariajuiit@gmail.com>
2025-02-22 13:40:13 +0600

    added

---
+++ js/third.js
@@ -0,0 +1,10 @@
+const main = document.getElementById('main-container')
+const section = document.createElement('section')
+section.innerHTML = `
+    <h1> My dynamic section </h1>
+    <p>Extra text added inside paragraph </p>
+    <ul>
+        <li>First item </li>
+    </ul>
+`
+main.appendChild(section)
